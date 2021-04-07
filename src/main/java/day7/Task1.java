76886447fa00dbeedd39b609f001072f716187fe
ggaivor1984 <grpanko@yandex.ru>
2021-04-07 20:30:51 +0300

    completedDay7

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -2,6 +2,8 @@ package day7;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Airplane a1 = new Airplane("Boeing", 2010, 800, 50000);
+        Airplane a2 = new Airplane("Airbus", 2004, 700, 40000);
+        Airplane.compareAirplanes(a1, a2);
     }
 }
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -1,7 +1,35 @@
 package day7;
 
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
+        Random rand = new Random();
+        Player player1 = new Player(rand.nextInt(Player.MAX_STAMINA - 90 + 1) + 90);
+        Player player2 = new Player(rand.nextInt(Player.MAX_STAMINA - 90 + 1) + 90);
+        Player player3 = new Player(rand.nextInt(Player.MAX_STAMINA - 90 + 1) + 90);
+        Player player4 = new Player(rand.nextInt(Player.MAX_STAMINA - 90 + 1) + 90);
+        Player player5 = new Player(rand.nextInt(Player.MAX_STAMINA - 90 + 1) + 90);
+        Player player6 = new Player(rand.nextInt(Player.MAX_STAMINA - 90 + 1) + 90);
+        /*Player player7 = new Player(rand.nextInt(11) + 90);
+        Player player8 = new Player(rand.nextInt(11) + 90);*/
+
+        //System.out.println(Player.getCountPlayers());
+        Player.info();
+
+        // System.out.println(player1.getStamina());
+        int staminaLevel = player4.getStamina();
+        for (int i = 0; i < staminaLevel; i++) player4.run();
+
+
+        //player1.run();
+        System.out.println("Stamina4 = " + player4.getStamina());
+        //System.out.println("Stamina1 = " + player1.getStamina());
+
+
+        System.out.println(Player.getCountPlayers());
+        Player.info();
+
 
     }
 }
