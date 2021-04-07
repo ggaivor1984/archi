67300b60a4e2f37b7858fed2dfe5151b4b6f2a7e
ggaivor1984 <grpanko@yandex.ru>
2021-04-07 21:31:30 +0300

    completedDay8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,24 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long start = System.nanoTime();
+
+        String result = "";
+        for (int i = 0; i <= 20000; i++) {
+            result = result + " " + i;
+        }
+        System.out.println(result);
+        long end = System.nanoTime();
+        long time = end - start;
+        System.out.println("Время работы программы String: " + time);
+
+        long start1 = System.nanoTime();
+        StringBuilder result1 = new StringBuilder("");
+        for (int i = 0; i <= 20000; i++) result1 = result1.append(i + " ");
+        System.out.println(result1);
+        long end1 = System.nanoTime();
+        long time1 = end1 - start1;
+        System.out.println("Время работы программы Stringbuilder: " + time1);
 
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,7 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Airplane airplane = new Airplane("SuperJet", 2011, 200, 400);
+        System.out.println(airplane);
     }
 }
