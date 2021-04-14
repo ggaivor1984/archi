5efd82f08f3deaaacd747ebcb3e968e28b5e4061
ggaivor1984 <grpanko@yandex.ru>
2021-04-14 21:42:53 +0300

    Day14Task1-2

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,41 @@
 package day14;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Task1 {
+
+    public static void main(String[] args) {
+        File myfile = new File("digits");
+        printSumDigits(myfile);
+    }
+
+    public static void printSumDigits(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            String fileDigits = scanner.nextLine();
+            String[] splitedDigits = fileDigits.split(" ");
+
+            if (splitedDigits.length < 10 || splitedDigits.length > 10) {
+                throw new IOException();
+            }
+            int[] myDigits = new int[10];
+            int i = 0;
+            for (String x : splitedDigits) {
+                myDigits[i++] = Integer.parseInt(x);
+            }
+            int sum = 0;
+            for (int j = 0; j < 10; j++) {
+                sum += myDigits[j];
+            }
+            System.out.println(sum);
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (java.io.IOException e) {
+            System.out.println("Некорректный входной файл");
+        }
     }
 }
--- src/main/java/day14/Task2.java
+++ src/main/java/day14/Task2.java
@@ -1,7 +1,37 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        File myfile = new File("people");
+        System.out.println(parseFileToStringList(myfile));
+    }
 
+    public static List<String> parseFileToStringList(File file) {
+        List<String> peopleList = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String s = scanner.nextLine();
+                peopleList.add(s);
+                String[] array = s.split(" ");
+                if (Integer.parseInt(array[1]) < 0) throw new IOException();
+            }
+            scanner.close();
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (java.io.IOException e) {
+            System.out.println("Некорректный входной файл");
+            return null;
+        }
+        return peopleList;
     }
 }
