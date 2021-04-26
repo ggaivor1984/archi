e3b6824faf24b11f2eda4268419982006deb89ad
ggaivor1984 <grpanko@yandex.ru>
2021-04-26 21:36:04 +0300

    Day19_Task2_completed

--- src/main/java/day19/Task2.java
+++ src/main/java/day19/Task2.java
@@ -1,10 +1,33 @@
 package day19;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * @author Neil Alishev
  */
 public class Task2 {
     public static void main(String[] args) {
-
+        File billion = new File("src/main/resources/numbers19.txt");
+        try {
+            Scanner scanner = new Scanner(billion);
+            Set<Integer> numbers = new HashSet<>();
+            //List<Integer> numbers1 = new ArrayList<>();
+            while (scanner.hasNext()) {
+                numbers.add(scanner.nextInt());
+                //numbers1.add(scanner.nextInt());
+            }
+            scanner.close();
+            //System.out.println(numbers);
+            int counter = 0;
+            for (int x : numbers) {
+                if (x >= 500000000 && x <= 600000000)
+                    counter++;
+            }
+            System.out.println("количество числе от 500 до 600 млн. равно: " + counter);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
